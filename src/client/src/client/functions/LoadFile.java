488c91f49a7a069cde173cfe5b3ba5fd80b69288
takantron89 <takantron89@users.noreply.github.com>
2018-09-28 11:21:02 -0700

    Update LoadFile.java

--- src/client/src/client/functions/LoadFile.java
+++ src/client/src/client/functions/LoadFile.java
@@ -1,10 +1,12 @@
 package client.functions;
 
 import java.awt.event.ActionEvent;
-
 import javax.swing.*;
+import java.io.*;
 
 public class LoadFile extends AbstractAction{
+	
+	FileInputStream in;
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
